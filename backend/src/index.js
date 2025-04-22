caf7476a88bab9ba970e07c033cc5e632a97d49d
simrann 02 <simran292002@gmail.com>
2025-04-22 14:04:09 +0530

    updated path for index file, setup rest client file, and auth routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,8 @@
 import express from 'express'
-
+import authRoutes from '../routes/auth.route.js'
 const app=express();
 const PORT=5000 
-
+app.use('/api/auth',authRoutes);
 app.listen(PORT, ()=>{
     console.log(`App is listening on port:${PORT}`);
 })
